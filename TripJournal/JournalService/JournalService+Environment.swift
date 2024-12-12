766d9ad070c9fc4b023363640a66c72b067dfc10
Hung Truong <hungtruongquoc@gmail.com>
2024-12-11 21:19:34 -0600

    Added a new service

--- TripJournal/JournalService/JournalService+Environment.swift
+++ TripJournal/JournalService/JournalService+Environment.swift
@@ -4,10 +4,20 @@ private struct JournalServiceKey: EnvironmentKey {
     static var defaultValue: JournalService = UnimplementedJournalService()
 }
 
+private struct JournalServiceLiveKey: EnvironmentKey {
+    static var defaultValue: JournalService = JournalServiceLive()
+}
+
 extension EnvironmentValues {
     /// A service that can used to interact with the trip journal API.
     var journalService: JournalService {
         get { self[JournalServiceKey.self] }
         set { self[JournalServiceKey.self] = newValue }
+    }
+    
+    /// Live implementation of the journal service
+    var journalServiceLive: JournalService {
+        get { self[JournalServiceLiveKey.self] }
+        set { self[JournalServiceLiveKey.self] = newValue }
     }
 }
