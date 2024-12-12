bc5552574b575076324aa0eef814c2807efdedba
Hung Truong <hungtruongquoc@gmail.com>
2024-12-11 18:57:59 -0600

    Added Codable protocol

--- TripJournal/Models/Models.swift
+++ TripJournal/Models/Models.swift
@@ -1,14 +1,14 @@
 import Foundation
 import MapKit
 
-/// Represents  a token that is returns when the user authenticates.
-struct Token {
+/// Represents a token that is returns when the user authenticates.
+struct Token: Codable {
     let accessToken: String
     let tokenType: String
 }
 
 /// Represents a trip.
-struct Trip: Identifiable, Sendable, Hashable {
+struct Trip: Identifiable, Sendable, Hashable, Codable {
     var id: Int
     var name: String
     var startDate: Date
@@ -17,7 +17,7 @@ struct Trip: Identifiable, Sendable, Hashable {
 }
 
 /// Represents an event in a trip.
-struct Event: Identifiable, Sendable, Hashable {
+struct Event: Identifiable, Sendable, Hashable, Codable {
     var id: Int
     var name: String
     var note: String?
@@ -28,18 +28,21 @@ struct Event: Identifiable, Sendable, Hashable {
 }
 
 /// Represents a location.
-struct Location: Sendable, Hashable {
+struct Location: Sendable, Hashable, Codable {
     var latitude: Double
     var longitude: Double
     var address: String?
-
+    
     var coordinate: CLLocationCoordinate2D {
         return .init(latitude: latitude, longitude: longitude)
     }
+    
+    // CLLocationCoordinate2D isn't Codable, but we don't need to encode it
+    // since it's a computed property derived from latitude and longitude
 }
 
 /// Represents a media with a URL.
-struct Media: Identifiable, Sendable, Hashable {
+struct Media: Identifiable, Sendable, Hashable, Codable {
     var id: Int
     var url: URL?
 }
